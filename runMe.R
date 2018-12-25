6a1cbcd155589298bc564f2ca73b45a000601f5a
Dr. Floren <mfloren@misericordia.edu>
2018-12-25 09:14:11 -0700

    changing back (testing update)

--- runMe.R
+++ runMe.R
@@ -2,4 +2,3 @@ list.of.packages <- c("shiny", "toOrdinal", "qboxplot", "DT", "R.utils", "aplpac
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, dependencies=TRUE)
 shiny::runGitHub("mth115-homework", username="mis-drfloren", launch.browser=TRUE)
-
