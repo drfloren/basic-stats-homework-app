0550637966c53f3a717f05e0bc9a203f6bd2ae39
Dr. Floren <mfloren@misericordia.edu>
2018-12-24 22:31:47 -0700

    Only install packages that aren't already installed.

--- runMe.R
+++ runMe.R
@@ -1,2 +1,4 @@
-install.packages(c("shiny", "toOrdinal", "qboxplot"))
+list.of.packages <- c("shiny", "toOrdinal", "qboxplot")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, dependencies=TRUE)
 shiny::runGitHub("mth115-homework", username="mis-drfloren", launch.browser=TRUE)
