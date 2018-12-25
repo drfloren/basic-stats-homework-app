e7ec9a3272c362a47ed35b7dbeed1108dddaecf9
Dr. Floren <mfloren@misericordia.edu>
2018-12-24 22:56:03 -0700

    Adding aplpack as a dependency

--- runMe.R
+++ runMe.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("shiny", "toOrdinal", "qboxplot", "DT", "R.utils")
+list.of.packages <- c("shiny", "toOrdinal", "qboxplot", "DT", "R.utils", "aplpack")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, dependencies=TRUE)
 shiny::runGitHub("mth115-homework", username="mis-drfloren", launch.browser=TRUE)
