3816d94a15f6fd364e8ec873324a683db48f8eac
Floren <mfloren@una.edu>
2022-01-11 17:30:55 -0600

    Adding dependency on xtable for chapter 4 and 5 tables

--- runMe.R
+++ runMe.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("htmltools", "shiny", "toOrdinal", "qboxplot", "DT", "R.utils", "aplpack")
+list.of.packages <- c("htmltools", "shiny", "toOrdinal", "qboxplot", "DT", "R.utils", "aplpack", "xtable")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, dependencies=TRUE)
 shiny::runGitHub("basic-stats-homework-app", username="drfloren", launch.browser=TRUE)
