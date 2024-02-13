e3b213c523da379db88022bf4a0bd0e76399185a
Michael <drmfloren@gmail.com>
2024-02-12 19:05:28 -0600

    Updating to try to get github to let me run this as a page

--- code_to_make_shinylive_site.R
+++ code_to_make_shinylive_site.R
@@ -1,1 +1,1 @@
-shinylive::export(appdir = "../Homework", destdir = "site")
+shinylive::export(appdir = "../Homework", destdir = "docs") #directions say "site", but using "docs" so I can try to publish on github (has to be in root or in docs)
